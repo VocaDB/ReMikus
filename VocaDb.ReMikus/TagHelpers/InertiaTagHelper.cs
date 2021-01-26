b8eed7196d4ba3833cfb72fa1408b1284c737fd9
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-01-26 23:30:40 +1000

    Use IJsonHelper.Serialize instead of JsonConvert.SerializeObject

--- VocaDb.ReMikus/TagHelpers/InertiaTagHelper.cs
+++ VocaDb.ReMikus/TagHelpers/InertiaTagHelper.cs
@@ -1,24 +1,23 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace VocaDb.ReMikus.TagHelpers
 {
 	[HtmlTargetElement("inertia", TagStructure = TagStructure.WithoutEndTag)]
 	public sealed class InertiaTagHelper : TagHelper
 	{
-		private static readonly JsonSerializerSettings s_settings = new()
-		{
-			ContractResolver = new CamelCasePropertyNamesContractResolver()
-		};
-
 		public object? Model { get; set; }
+
+		[ViewContext]
+		public ViewContext ViewContext { get; set; } = default!;
 
 		public override void Process(TagHelperContext context, TagHelperOutput output)
 		{
 			output.TagName = "div";
 			output.Attributes.SetAttribute("id", "app");
-			output.Attributes.SetAttribute("data-page", JsonConvert.SerializeObject(Model, s_settings));
+			output.Attributes.SetAttribute("data-page", ViewContext.HttpContext.RequestServices.GetRequiredService<IJsonHelper>().Serialize(Model).ToString()/* required */);
 			output.TagMode = TagMode.StartTagAndEndTag;
 		}
 	}
